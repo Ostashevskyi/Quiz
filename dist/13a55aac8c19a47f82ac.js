dc4bc535b45318951742483cd7cb2c2b4464d02c
Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-03-30 23:09:30 +0300

    Make enviroment settings

---
+++ dist/13a55aac8c19a47f82ac.js
@@ -0,0 +1,1 @@
+import"./index.html";import"./index.css";const res=(o,s)=>o*s;console.log(res(1,2));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.html'
+import './index.scss'
+
+const res = (a,b) => a * b;
+
+console.log(res(1,2));
